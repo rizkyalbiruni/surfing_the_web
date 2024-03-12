9596d0b8f48f499a18217f24738fdb64471f0bea
Charlie Wilson <cwhwilson@hotmail.co.uk>
2024-03-12 13:38:01 +0000

    seeds set up, awaiting Muhammad changes to schema syntax (eg type)

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,19 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+ require 'faker'
+
+puts "Cleaning up database..."
+Surfboard.destroy_all
+puts "Database cleaned"
+
+
+ 20.times do |i|
+  Surfboard.create(type: Faker::Creature::Animal.name,
+  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ornare at elit non accumsan. Fusce sapien nisl, viverra in justo egestas, volutpat convallis lectus. Aliquam ut ipsum nec dolor molestie sagittis. Suspendisse volutpat laoreet ipsum eget pulvinar. Mauris sit amet lectus eget nisl cursus elementum sit amet ac massa. Mauris",
+  img_url: "https://source.unsplash.com/random?surfing", location: Faker::Address.city)
+end
+
+20.times do |i|
+  User.create(name: Faker::Name.name, email: Faker::Internet.email, encrypted_password: Faker::Internet.password  )
+end
